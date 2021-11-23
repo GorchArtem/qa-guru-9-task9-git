d30f61ccb1f70c51918bea091b7efa65e0c2e2a5
Артемий <rick-glat@mail.ru>
2021-11-23 15:08:46 +0300

    add closeWebDriver

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -1,12 +1,21 @@
 package tests;
 
 import com.codeborne.selenide.Configuration;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeAll;
+
+import static com.codeborne.selenide.WebDriverRunner.closeWebDriver;
 
 public class TestBase {
     @BeforeAll
     static void beforeAll() {
         Configuration.browserSize = "1366x768";
         Configuration.pageLoadStrategy = "eager";
+    }
+
+    @AfterEach
+    void afterEach() {
+        closeWebDriver();
     }
 }
